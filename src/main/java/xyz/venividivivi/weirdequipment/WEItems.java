d242578e50f22d66fbaee5b16a807a5fc10ae0ef
Vivian Rose <VioletRose@VeniVidiVivi.xyz>
2022-06-17 03:02:17 -0500

    Added torch core, crafting recipes

--- src/main/java/xyz/venividivivi/weirdequipment/WEItems.java
+++ src/main/java/xyz/venividivivi/weirdequipment/WEItems.java
@@ -12,17 +12,30 @@ import xyz.venividivivi.weirdequipment.materials.CactusMaterial;
 import xyz.venividivivi.weirdequipment.materials.PumpkinArmorMaterial;
 
 public class WEItems {
+    //Weapons
     public static final Item CACTUS_SWORD = new CactusSword(new CactusMaterial(), 1, -2f, new FabricItemSettings().group(ItemGroup.COMBAT));
     public static final Item DIRT_SWORD = new SwordItem(ToolMaterials.WOOD, -3, -2.6f, new FabricItemSettings().group(ItemGroup.COMBAT));
+
+    //Tools
     public static final Item NETHERITE_TORCH_PICKAXE = new NetheriteTorchPickaxe(ToolMaterials.NETHERITE, 1, -2.8f, new FabricItemSettings().group(ItemGroup.TOOLS));
-    public static final Item JACK_O_HELMET = new ArmorItem(new PumpkinArmorMaterial(), EquipmentSlot.HEAD, new FabricItemSettings().group(ItemGroup.COMBAT));
+
     public static final Item FLINT_AND_SHEARS = new FlintAndShears(new FabricItemSettings().maxDamage(300).group(ItemGroup.TOOLS));
+    //Armor
+    public static final Item JACK_O_HELMET = new ArmorItem(new PumpkinArmorMaterial(), EquipmentSlot.HEAD, new FabricItemSettings().group(ItemGroup.COMBAT));
+
+    //Resources
+    public static final Item TORCH_CORE = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
 
     public static void register() {
+        //Weapons
         Registry.register(Registry.ITEM, new Identifier("weird_equipment", "cactus_sword"), CACTUS_SWORD);
         Registry.register(Registry.ITEM, new Identifier("weird_equipment", "dirt_sword"), DIRT_SWORD);
+        //Tools
         Registry.register(Registry.ITEM, new Identifier("weird_equipment", "netherite_torch_pickaxe"), NETHERITE_TORCH_PICKAXE);
-        Registry.register(Registry.ITEM, new Identifier("weird_equipment", "jack_o_helmet"), JACK_O_HELMET);
         Registry.register(Registry.ITEM, new Identifier("weird_equipment", "flint_and_shears"), FLINT_AND_SHEARS);
+        //Armor
+        Registry.register(Registry.ITEM, new Identifier("weird_equipment", "jack_o_helmet"), JACK_O_HELMET);
+        //Resources
+        Registry.register(Registry.ITEM, new Identifier("weird_equipment", "torch_core"), TORCH_CORE);
     }
 }
