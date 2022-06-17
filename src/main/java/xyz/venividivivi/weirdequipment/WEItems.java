9f8a945ac072583927f90ecd8e6f0f6cffaeb850
Vivian Rose <VioletRose@VeniVidiVivi.xyz>
2022-06-17 01:17:20 -0500

    Added dirt sword and mixin to allow giving the sword to zombies

--- src/main/java/xyz/venividivivi/weirdequipment/WEItems.java
+++ src/main/java/xyz/venividivivi/weirdequipment/WEItems.java
@@ -12,10 +12,13 @@ import xyz.venividivivi.weirdequipment.materials.PumpkinArmorMaterial;
 
 public class WEItems {
     public static final Item CACTUS_SWORD = new CactusSword(new CactusMaterial(), 1, -2f, new FabricItemSettings().group(ItemGroup.COMBAT));
+    public static final Item DIRT_SWORD = new SwordItem(ToolMaterials.WOOD, -3, -2.6f, new FabricItemSettings().group(ItemGroup.COMBAT));
     public static final Item JACK_O_HELMET = new ArmorItem(new PumpkinArmorMaterial(), EquipmentSlot.HEAD, new FabricItemSettings().group(ItemGroup.COMBAT));
     public static final Item FLINT_AND_SHEARS = new FlintAndShears(new FabricItemSettings().maxDamage(300).group(ItemGroup.TOOLS));
+
     public static void register() {
         Registry.register(Registry.ITEM, new Identifier("weird_equipment", "cactus_sword"), CACTUS_SWORD);
+        Registry.register(Registry.ITEM, new Identifier("weird_equipment", "dirt_sword"), DIRT_SWORD);
         Registry.register(Registry.ITEM, new Identifier("weird_equipment", "jack_o_helmet"), JACK_O_HELMET);
         Registry.register(Registry.ITEM, new Identifier("weird_equipment", "flint_and_shears"), FLINT_AND_SHEARS);
     }
