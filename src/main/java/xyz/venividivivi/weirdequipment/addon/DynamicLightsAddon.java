1030b815a4bd1a7030139993c3a784416bb336a7
Vivian Rose <VioletRose@VeniVidiVivi.xyz>
2022-06-20 03:36:39 -0500

    Removed erroneous EntityType casting in Lambdynamiclights addon

--- src/main/java/xyz/venividivivi/weirdequipment/addon/DynamicLightsAddon.java
+++ src/main/java/xyz/venividivivi/weirdequipment/addon/DynamicLightsAddon.java
@@ -2,12 +2,11 @@ package xyz.venividivivi.weirdequipment.addon;
 
 import dev.lambdaurora.lambdynlights.api.DynamicLightHandlers;
 import dev.lambdaurora.lambdynlights.api.DynamicLightsInitializer;
-import net.minecraft.entity.EntityType;
 import xyz.venividivivi.weirdequipment.registry.WeirdEquipmentEntityTypes;
 
 public class DynamicLightsAddon implements DynamicLightsInitializer {
     @Override
     public void onInitializeDynamicLights() {
-        DynamicLightHandlers.registerDynamicLightHandler((EntityType) WeirdEquipmentEntityTypes.TORCH_ARROW, lightSource -> 12);
+        DynamicLightHandlers.registerDynamicLightHandler(WeirdEquipmentEntityTypes.TORCH_ARROW, lightSource -> 12);
     }
 }
