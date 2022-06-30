828b843269a114019a142e0f85d1b4376e3c8f1e
Vivian Rose <VioletRose@VeniVidiVivi.xyz>
2022-06-30 02:22:55 -0500

    Optimized NetheriteTorchPickaxeItem

--- src/main/java/xyz/venividivivi/weirdequipment/item/NetheriteTorchPickaxeItem.java
+++ src/main/java/xyz/venividivivi/weirdequipment/item/NetheriteTorchPickaxeItem.java
@@ -28,31 +28,23 @@ public class NetheriteTorchPickaxeItem extends PickaxeItem {
         Direction side = context.getSide();
         BlockPos blockPos = context.getBlockPos().offset(side);
         BlockState blockState;
-        Block block;
-            switch(side) {
-                case NORTH, EAST, SOUTH, WEST:
-                    block = Blocks.WALL_TORCH;
-                    blockState = block.getDefaultState().with(WallTorchBlock.FACING, side);
-                    break;
-                default:
-                    block = Blocks.TORCH;
-                    blockState = block.getDefaultState();
-                    if (!Blocks.TORCH.canPlaceAt(blockState, world, blockPos)) {
-                        blockState = Blocks.TORCH.getDefaultState();
-                    }
+        if (side.getAxis().isHorizontal()) {
+            blockState = Blocks.WALL_TORCH.getDefaultState().with(WallTorchBlock.FACING, side);
+        } else {
+            blockState = Blocks.TORCH.getDefaultState();
+        }
+        if (blockState.canPlaceAt(world, blockPos)) {
+            PlayerEntity playerEntity = context.getPlayer();
+            world.playSound(playerEntity, blockPos, SoundEvents.BLOCK_WOOD_PLACE, SoundCategory.BLOCKS, 1.0F, 0.9f);
+            world.setBlockState(blockPos, blockState);
+            world.emitGameEvent(playerEntity, GameEvent.BLOCK_CHANGE, blockPos);
+            if (playerEntity instanceof ServerPlayerEntity) {
+                context.getStack().damage(5, playerEntity, (p) -> {
+                    p.sendToolBreakStatus(context.getHand());
+                });
             }
-            if (block.canPlaceAt(blockState, world, blockPos)) {
-                PlayerEntity playerEntity = context.getPlayer();
-                world.playSound(playerEntity, blockPos, SoundEvents.BLOCK_WOOD_PLACE, SoundCategory.BLOCKS, 1.0F, 0.9f);
-                world.setBlockState(blockPos, blockState);
-                world.emitGameEvent(playerEntity, GameEvent.BLOCK_CHANGE, blockPos);
-                if (playerEntity instanceof ServerPlayerEntity) {
-                    context.getStack().damage(5, playerEntity, (p) -> {
-                        p.sendToolBreakStatus(context.getHand());
-                    });
-                }
-                return ActionResult.SUCCESS;
-            }
-        return super.useOnBlock(context);
+            return ActionResult.SUCCESS;
+        }
+        return ActionResult.PASS;
     }
 }
